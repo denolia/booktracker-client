7f94e0fd01736e458318f6a53633366d53b7502f
Julia Bubnova <bubnova.j.i@gmail.com>
2022-12-14 21:52:44 +0100

    Add get book by id mock

--- mock_server/run.js
+++ mock_server/run.js
@@ -2,32 +2,38 @@ const express = require('express');
 const app = express();
 const port = 3003;
 
-app.get('/books', (req, res) => {
-  const books = [
-    {
-      id: 1,
-      name: 'The Great Gatsby',
-      author: 'F. Scott Fitzgerald',
-      description: 'asdf',
-      progress: '20',
-    },
-    {
-      id: 2,
-      name: 'To Kill a Mockingbird',
-      author: 'Harper Lee',
-      description: 'asdf',
-      progress: '30',
-    },
-    {
-      id: 3,
-      name: 'One Hundred Years of Solitude',
-      author: 'Gabriel García Márquez',
-      description: 'asdf',
-      progress: '90',
-    },
-  ];
+const books = [
+  {
+    id: '1',
+    name: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    description: 'asdf',
+    progress: '20',
+  },
+  {
+    id: '2',
+    name: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    description: 'asdf',
+    progress: '30',
+  },
+  {
+    id: '3',
+    name: 'One Hundred Years of Solitude',
+    author: 'Gabriel García Márquez',
+    description: 'asdf',
+    progress: '90',
+  },
+];
 
+app.get('/books', (req, res) => {
   res.send(books);
+});
+
+app.get('/book', function (req, res) {
+  const id = req.query.id;
+  console.log('params: ', req.query);
+  res.send(books.find((book) => book.id === id));
 });
 
 app.get('/', (req, res) => {
