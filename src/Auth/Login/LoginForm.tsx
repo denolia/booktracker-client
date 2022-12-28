7c7bb23397c63f172235b6b4d0659c6bda34a0ef
Julia Bubnova <bubnova.j.i@gmail.com>
2022-12-28 17:04:17 +0100

    [#47] use login/sign up template

--- src/Auth/Login/LoginForm.tsx
+++ src/Auth/Login/LoginForm.tsx
@@ -1,7 +1,35 @@
 import React, { FormEvent, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { useAuth } from '../AuthContext';
 import { LoginMode } from './types';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+function Copyright(props: any) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      Copyright ©
+      <Link color="inherit" href="https://juliabubnova.com/">
+        Book Tracker
+      </Link>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
+  );
+}
 
 interface Props {
   mode: LoginMode;
@@ -10,56 +38,96 @@ interface Props {
 export function LoginForm({ mode }: Props) {
   const { login, signup } = useAuth();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    const data = new FormData(e.currentTarget);
+
+    const email = data.get('email') as string | undefined;
+    const password = data.get('password') as string | undefined;
+
     const loginMethod = mode === LoginMode.SIGN_IN ? login : signup;
-    loginMethod(email, password);
+    if (email && password) {
+      loginMethod(email, password);
+    }
   }
 
   return (
     <div className="container-sm justify-content-center col-lg-4 col-md-8 col-sm-12">
-      <h4>
-        {mode} or{' '}
-        <Link
-          to={mode === LoginMode.SIGN_IN ? '/register' : '/login'}
-          className="link"
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
         >
-          {mode === LoginMode.SIGN_IN ? LoginMode.SIGN_UP : LoginMode.SIGN_IN}
-        </Link>
-      </h4>
-
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="inputEmail">Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            id="inputEmail"
-            aria-describedby="emailHelp"
-            placeholder="Enter your email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value as string)}
-          />
-        </div>
-
-        <div className="form-group">
-          <label htmlFor="inputPassword">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="inputPassword"
-            value={password}
-            onChange={(e) => setPassword(e.target.value as string)}
-          />
-        </div>
-
-        <div className="form-group">
-          <input type="submit" value={mode} className="btn btn-primary" />
-        </div>
-      </form>
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            {mode === LoginMode.SIGN_IN ? LoginMode.SIGN_UP : LoginMode.SIGN_IN}
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link
+                  href={mode === LoginMode.SIGN_IN ? '/register' : '/login'}
+                  variant="body2"
+                >
+                  {mode === LoginMode.SIGN_IN
+                    ? "Don't have an account? Sign Up"
+                    : 'Already have an account? Sign In'}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
     </div>
   );
 }
