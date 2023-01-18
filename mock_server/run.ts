c11eecf5397d440774efe5113c5332efa1309ef8
Julia Bubnova <bubnova.j.i@gmail.com>
2023-01-19 00:44:08 +0100

    Migrate the mock to ts, use tsx to run it

--- mock_server/run.ts
+++ mock_server/run.ts
@@ -1,5 +1,7 @@
-const express = require('express');
-const cors = require('cors');
+/* eslint-disable import/no-extraneous-dependencies */
+import express from 'express';
+import cors from 'cors';
+
 const app = express();
 const port = 3003;
 
